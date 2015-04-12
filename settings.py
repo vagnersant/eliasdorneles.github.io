05c2b3a77a19ac53fb7d82e389d595498c583f04
Elias Dorneles <eliasdorneles@gmail.com>
2015-04-11 23:04:55 -0300

    adjusting file name generation, adding timezone and github url

--- settings.py
+++ settings.py
@@ -3,6 +3,16 @@ from __future__ import unicode_literals
 
 SITENAME = 'Hopeful Ramble'
 
-ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+FILENAME_METADATA = '(?P<name>[^/]+)'
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{name}.html'
 
 ARTICLE_SAVE_AS = ARTICLE_URL
+
+
+# I'm the only author
+AUTHOR_SAVE_AS = ''
+
+TIMEZONE = 'America/Sao_Paulo'
+
+GITHUB_URL = 'https://github.com/eliasdorneles'
