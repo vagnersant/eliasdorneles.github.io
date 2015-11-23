723958f5b8ebfb14f5bd684b61e6ef437348bc57
Elias Dorneles <eliasdorneles@gmail.com>
2015-11-22 22:40:11 -0200

    take 8

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-eval $(ssh-agent) && chmod 600 deploy_key.pem && ssh-add deploy_key.pem
-
-git remote add deploy git@github.com:eliasdorneles/eliasdorneles.github.io.git || echo "Remote already exists"
-git remote -v
-
 make clean compile
 
 ghp-import -m "Update site" output
